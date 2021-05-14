f51c3f3520938e6c60a7e74205874227acceae54
Rafael Costa <rafael.costa@connectcom.com.br>
2021-05-14 17:41:00 -0300

    Change WallerController folder

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\WalletController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,7 @@ use App\Http\Controllers\API\AuthController;
 */
 
 Route::get('/authenticate', [AuthController::class, 'authenticate']);
+Route::get('/wallet', [WallerController::class, 'get']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
