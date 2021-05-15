8c1971184827224348c3d24e2a77a4563a4e66da
Rafael Costa <rafael.costa@connectcom.com.br>
2021-05-15 17:55:44 -0300

    Changes ApiKeySeeders to make it static for testing purposes

--- database/seeders/ApiKeysTableSeeder.php
+++ database/seeders/ApiKeysTableSeeder.php
@@ -20,31 +20,31 @@ class ApiKeysTableSeeder extends Seeder
         $apiKey = ApiKey::updateOrCreate([
             'user_id' => 1,
             'status'  => ApiKeyStatusConstant::ACTIVE,
-            'hash'	  => Str::random(30)
+            'hash'	  => 'mLgeVH4aYah0O6y2TqZlwkinGqXBRn' // Should be Str::random(30), it is static now just to make it easier to provide API documentation
         ]);
 
         $apiKey = ApiKey::updateOrCreate([
             'user_id' => 2,
             'status'  => ApiKeyStatusConstant::ACTIVE,
-            'hash'	  => Str::random(30)
+            'hash'	  => 'SnuaSUD32Ay0esYgI4o08bsZWtlOj7' // Should be Str::random(30), it is static now just to make it easier to provide API documentationStr::random(30)
         ]);
 
         $apiKey = ApiKey::updateOrCreate([
             'user_id' => 3,
             'status'  => ApiKeyStatusConstant::ACTIVE,
-            'hash'	  => Str::random(30)
+            'hash'	  => 'HIZ0fgGzP2RoaI0GpvhHo4RbIrq9m3' // Should be Str::random(30), it is static now just to make it easier to provide API documentationStr::random(30)
         ]);
 
         $apiKey = ApiKey::updateOrCreate([
             'user_id' => 4,
             'status'  => ApiKeyStatusConstant::ACTIVE,
-            'hash'	  => Str::random(30)
+            'hash'	  => 'UsWWWfDuhwIvrGepd4sxvsVcRsF5Ig' // Should be Str::random(30), it is static now just to make it easier to provide API documentationStr::random(30)
         ]);
 
         $apiKey = ApiKey::updateOrCreate([
             'user_id' => 5,
             'status'  => ApiKeyStatusConstant::ACTIVE,
-            'hash'	  => Str::random(30)
+            'hash'	  => 'gqZBymbN2dVJZ71Tll5vm6S74LKfxz' // Should be Str::random(30), it is static now just to make it easier to provide API documentationStr::random(30)
         ]);
     }
 }
